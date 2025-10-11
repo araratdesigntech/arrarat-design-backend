ed9135682140f887e946750cdbf70d0880d8a44b
CounselJS <okpabicounsel@gmail.com>
2025-10-11 01:15:49 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,12 @@ app.use(morgan('dev'));
 app.use(helmet());
 
 const corsOptions = {
-  origin: ['http://localhost:5500', 'http://localhost:5501', 'https://arrarat-designs.onrender.com'],
+  origin: [
+    'http://localhost:5500',
+    'http://127.0.0.1:5501',
+    'http://localhost:5501',
+    'https://arrarat-designs.onrender.com',
+  ],
   methods: 'GET, PUT, POST, DELETE, OPTIONS',
   credentials: true,
 };
